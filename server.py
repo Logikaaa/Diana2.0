4115759fce244422c878555051e7814788b49412
Logikaaa <123231827+Logikaaa@users.noreply.github.com>
2025-05-18 15:29:38 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,37 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 12345))
+
+server_socket.listen(1)
+connection, address = server_socket.accept()
+client_name = connection.recv(1024).decode()
+connection.send(f"Вітаю{client_name} на сервері.".encode())
+
+command = connection.recv(1024).decode()
+if command == "NAME":
+    connection.send(f"Твоє ім'я:{client_name}".encode())
+elif command == "EXIT":
+    connection.send(f"Бувай{client_name}".encode())
+    connection.close()
+else:
+    connection.send("Такої команди не існує".encode())
+    
+server_socket.close()
+    
+
+print('Сервер очікує підключення')
+
+
+
+print(f'Підключився клієнт: {address}')
+
+data = connection.recv(1024).decode()
+
+print(f'Отримано повідомлення від клієнта: {data}')
+
+connection.send('Привіт від сервера!'.encode())
+
+connection.close()
+
+server_socket.close()
